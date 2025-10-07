e0e5a8dac0d3eab5fb7eb944c494e59f31e6a91d
“Dayyan <[dayyanfahad20@gmail.com]>
2025-10-07 19:33:27 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+let task =document.getElementById("task");
+function clearInputField(){
+    task.value ="";
+}
+
+function saveTask() {
+    let mainDiv =document.getElementById("div-main");
+    mainDiv.style.display ="block";
+    let paragraph =document.createElement("p");
+    paragraph.classList.add('p');
+    let newDiv =document.createElement("div");
+    newDiv.classList.add('divproperties');
+     
+    let checkbox = document.createElement("input");
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('checkbox1');
+    let label =document.createElement("label");
+    label.textContent = task.value ;
+    checkbox.onclick = function(){
+        if(checkbox.checked){
+            label.style.textDecoration = 'line-through';
+            checkbox.style.display ='none';
+            editButton.remove();
+            let complete = document.createElement('p');
+            let comp_text =document.createTextNode("Completed");
+            complete.appendChild(comp_text);
+            complete.classList.add('completetext')
+            newDiv.appendChild(complete);
+            newDiv.style.marginLeft ='20px';
+        }
+    }
+    
+    let editButton =document.createElement("button");
+    editButton.classList.add('Button' , 'editbutton');
+    editButton.onclick = function (){
+        task.value =label.textContent;
+        task.focus();
+        newDiv.remove();
+    };
+    
+    let editicon = document.createElement("img");
+    editicon.src ="edit-icon.png";
+    editicon.classList.add("img");
+    
+    let deleteButton =document.createElement("button");
+    deleteButton.classList.add('Button');
+    deleteButton.onclick =function(){
+        newDiv.remove();
+    }
+    
+    let del_icon = document.createElement("img");
+    del_icon.src ="del-icon.png";
+    del_icon.classList.add("img");
+    
+    // let paragraphText =document.createTextNode(task);
+    
+    // let editButtonText =document.createTextNode(editicon);
+    // let delButtonText = document.createTextNode("Delete Task")
+    paragraph.appendChild(checkbox);
+    paragraph.appendChild(label);
+    editButton.appendChild(editicon);
+    deleteButton.appendChild(del_icon);
+    newDiv.appendChild(paragraph);
+    newDiv.appendChild(editButton);
+    newDiv.appendChild(deleteButton);
+    mainDiv.appendChild(newDiv);
+}
